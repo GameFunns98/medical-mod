2dfd9788ff4079c73e8bc1bad1a54361ef9076be
Václav Duda <99597281+GameFunns98@users.noreply.github.com>
2026-06-12 19:33:30 +0200

    feat: register GUI and medical supplies

--- src/main/java/cz/vasek/medicalmod/MedicalMod.java
+++ src/main/java/cz/vasek/medicalmod/MedicalMod.java
@@ -2,6 +2,7 @@ package cz.vasek.medicalmod;
 
 import com.mojang.logging.LogUtils;
 import cz.vasek.medicalmod.registry.ModItems;
+import cz.vasek.medicalmod.registry.ModMenus;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,14 +19,28 @@ public final class MedicalMod {
         IEventBus modEventBus = context.getModEventBus();
 
         ModItems.register(modEventBus);
+        ModMenus.register(modEventBus);
         modEventBus.addListener(this::addCreativeTabItems);
 
         LOGGER.info("Medical Mod is loading.");
     }
 
     private void addCreativeTabItems(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.MEDICAL_TABLET);
+            event.accept(ModItems.DEFIBRILLATOR);
+        }
+
         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.BANDAGE);
+            event.accept(ModItems.TOURNIQUET);
+            event.accept(ModItems.SPLINT);
+            event.accept(ModItems.OXYGEN_MASK);
+            event.accept(ModItems.MORPHINE);
+            event.accept(ModItems.EPINEPHRINE);
+            event.accept(ModItems.SALINE);
+            event.accept(ModItems.CHEST_SEAL);
+            event.accept(ModItems.DECOMPRESSION_NEEDLE);
         }
     }
 }
