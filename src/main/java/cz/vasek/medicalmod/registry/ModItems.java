d6f20ae68e36faf50fda4bbc1fb215954b65e27f
Václav Duda <99597281+GameFunns98@users.noreply.github.com>
2026-06-12 19:32:22 +0200

    feat: register complete medical equipment set

--- src/main/java/cz/vasek/medicalmod/registry/ModItems.java
+++ src/main/java/cz/vasek/medicalmod/registry/ModItems.java
@@ -2,6 +2,7 @@ package cz.vasek.medicalmod.registry;
 
 import cz.vasek.medicalmod.MedicalMod;
 import cz.vasek.medicalmod.item.BandageItem;
+import cz.vasek.medicalmod.item.MedicalTabletItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,9 +13,59 @@ public final class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MedicalMod.MOD_ID);
 
+    public static final RegistryObject<Item> MEDICAL_TABLET = ITEMS.register(
+            "medical_tablet",
+            () -> new MedicalTabletItem(new Item.Properties().stacksTo(1))
+    );
+
     public static final RegistryObject<Item> BANDAGE = ITEMS.register(
             "bandage",
             () -> new BandageItem(new Item.Properties().stacksTo(16))
+    );
+
+    public static final RegistryObject<Item> TOURNIQUET = ITEMS.register(
+            "tourniquet",
+            () -> new Item(new Item.Properties().stacksTo(8))
+    );
+
+    public static final RegistryObject<Item> SPLINT = ITEMS.register(
+            "splint",
+            () -> new Item(new Item.Properties().stacksTo(8))
+    );
+
+    public static final RegistryObject<Item> OXYGEN_MASK = ITEMS.register(
+            "oxygen_mask",
+            () -> new Item(new Item.Properties().stacksTo(4))
+    );
+
+    public static final RegistryObject<Item> MORPHINE = ITEMS.register(
+            "morphine",
+            () -> new Item(new Item.Properties().stacksTo(16))
+    );
+
+    public static final RegistryObject<Item> EPINEPHRINE = ITEMS.register(
+            "epinephrine",
+            () -> new Item(new Item.Properties().stacksTo(16))
+    );
+
+    public static final RegistryObject<Item> SALINE = ITEMS.register(
+            "saline",
+            () -> new Item(new Item.Properties().stacksTo(8))
+    );
+
+    public static final RegistryObject<Item> CHEST_SEAL = ITEMS.register(
+            "chest_seal",
+            () -> new Item(new Item.Properties().stacksTo(16))
+    );
+
+    public static final RegistryObject<Item> DECOMPRESSION_NEEDLE = ITEMS.register(
+            "decompression_needle",
+            () -> new Item(new Item.Properties().stacksTo(16))
+    );
+
+    public static final RegistryObject<Item> DEFIBRILLATOR = ITEMS.register(
+            "defibrillator",
+            () -> new Item(new Item.Properties().stacksTo(1))
     );
 
     private ModItems() {
