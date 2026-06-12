1be2e28c6ae254800c3b0e2254e70c21fe7b5b79
Václav Duda <99597281+GameFunns98@users.noreply.github.com>
2026-06-12 19:33:14 +0200

    feat: register medical screen on client

---
+++ src/main/java/cz/vasek/medicalmod/client/ClientModEvents.java
@@ -0,0 +1,27 @@
+package cz.vasek.medicalmod.client;
+
+import cz.vasek.medicalmod.MedicalMod;
+import cz.vasek.medicalmod.registry.ModMenus;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(
+        modid = MedicalMod.MOD_ID,
+        bus = Mod.EventBusSubscriber.Bus.MOD,
+        value = Dist.CLIENT
+)
+public final class ClientModEvents {
+    private ClientModEvents() {
+    }
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> MenuScreens.register(
+                ModMenus.MEDICAL_MENU.get(),
+                MedicalScreen::new
+        ));
+    }
+}
