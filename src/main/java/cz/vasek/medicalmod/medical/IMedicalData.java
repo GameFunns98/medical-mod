2597de830f618866df307429b68cb1d9412a013e
Václav Duda <99597281+GameFunns98@users.noreply.github.com>
2026-06-12 19:30:07 +0200

    feat: expand medical data contract

--- src/main/java/cz/vasek/medicalmod/medical/IMedicalData.java
+++ src/main/java/cz/vasek/medicalmod/medical/IMedicalData.java
@@ -8,6 +8,66 @@ public interface IMedicalData {
 
     void setBleedingLevel(int level);
 
+    int getPain();
+
+    void setPain(int pain);
+
+    int getConsciousnessLevel();
+
+    void setConsciousnessLevel(int level);
+
+    int getPulse();
+
+    void setPulse(int pulse);
+
+    int getSystolicPressure();
+
+    void setSystolicPressure(int pressure);
+
+    int getDiastolicPressure();
+
+    void setDiastolicPressure(int pressure);
+
+    int getOxygenSaturation();
+
+    void setOxygenSaturation(int saturation);
+
+    int getRespiratoryRate();
+
+    void setRespiratoryRate(int rate);
+
+    int getTemperatureTenths();
+
+    void setTemperatureTenths(int temperatureTenths);
+
+    int getBloodVolumeMl();
+
+    void setBloodVolumeMl(int bloodVolumeMl);
+
+    int getAirwayStatus();
+
+    void setAirwayStatus(int status);
+
+    boolean hasPneumothorax();
+
+    void setPneumothorax(boolean pneumothorax);
+
+    int getCardiacRhythm();
+
+    void setCardiacRhythm(int rhythm);
+
+    boolean hasIvAccess();
+
+    void setIvAccess(boolean ivAccess);
+
+    int getInjurySeverity(BodyPart bodyPart);
+
+    void setInjurySeverity(BodyPart bodyPart, int severity);
+
+    boolean hasFracture(BodyPart bodyPart);
+
+    void setFracture(BodyPart bodyPart, boolean fractured);
+
     default boolean isBleeding() {
         return getBleedingLevel() > 0;
     }
